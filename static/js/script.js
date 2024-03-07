21e18da587c41f8b2bacb0a3d466b567a80d7344
Ashot Hambardzumyan <avhmail@gmail.com>
2024-03-07 23:43:33 +0400

    test folders request

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch('https://records-slow-control.app.cern.ch/get_folders')
+  fetch('https://records-slow-control.app.cern.ch/files/2024-03-06_14-16-28/cam401')
   .then((response) => {
     return response.json();
   })
