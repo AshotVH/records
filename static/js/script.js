07788af3df1016b586a72ade273dc3623c64eff9
Ashot Hambardzumyan <avhmail@gmail.com>
2024-03-10 00:34:58 +0400

    24.03.10

--- static/js/script.js
+++ static/js/script.js
@@ -109,91 +109,91 @@ async function getFolders() {
 }
 document.addEventListener("DOMContentLoaded", () => {
   
-  // getFolders().then((data) => {
-  //   console.log(Object.keys(data).length);
-  //   let treeData = [];
-  //   for (const [key, value] of Object.entries(data)) {
-  //     if (value.length == 7) {
-  //       folderTimestamps.push(toLocalTimeStr(key)[1]);
-  //       let node = {
-  //         text: toLocalTimeStr(key)[0],
-  //         icon: "fa fa-folder",
-  //         nodes: [
-  //           {
-  //             text: "cam-401",
-  //             icon: "fa-regular fa-image",
-  //             id: key + " cam401",
-  //             class: "cam_item",
-  //           },
-  //           {
-  //             text: "cam-404",
-  //             icon: "fa-regular fa-image",
-  //             id: key + " cam404",
-  //             class: "cam_item",
-  //           },
-  //           {
-  //             text: "cam-405",
-  //             icon: "fa-regular fa-image",
-  //             id: key + " cam405",
-  //             class: "cam_item",
-  //           },
-  //           {
-  //             text: "cam-407",
-  //             icon: "fa-regular fa-image",
-  //             id: key + " cam407",
-  //             class: "cam_item",
-  //           },
-  //           {
-  //             text: "cam-408",
-  //             icon: "fa-regular fa-image",
-  //             id: key + " cam408",
-  //             class: "cam_item",
-  //           },
-  //           {
-  //             text: "cam-409",
-  //             icon: "fa-regular fa-image",
-  //             id: key + " cam409",
-  //             class: "cam_item",
-  //           },
-  //           {
-  //             text: "cam-410",
-  //             icon: "fa-regular fa-image",
-  //             id: key + " cam410",
-  //             class: "cam_item",
-  //           },
-  //         ],
-  //       };
-  //       treeData.push(node);
-  //     }
-  //   }
-  //   console.log(treeData.length);
-  //   folderTimestamps.sort((a,b) => a - b);
-  //   console.log(folderTimestamps);
-  //   console.log(folderTimestamps.length);
-  //   $("#tree").bstreeview({
-  //     data: treeData,
-  //   });
-  //   $(".cam_item").on("click", function (event) {
-  //     console.log(event.target.id);
-  //     event.stopPropagation();
-  //     [timeStamp, filename] = event.target.id.split(" ");
-  //     fetch(
-  //       `https://records-np04-slow-control.app.cern.ch/files/${timeStamp}/${filename}`
-  //     )
-  //       .then((response) => response.text())
-  //       .then((data) => {
-  //         const element = document.getElementById("screenshot");
-  //         if (element) {
-  //           element.remove();
-  //         }
-  //         const img = document.createElement("img");
-  //         img.setAttribute("id", "screenshot");
-  //         img.src = "data:image/jpeg;base64," + data;
-  //         document.getElementsByClassName("img_wrapper")[0].appendChild(img);
-  //       })
-  //       .catch((error) => console.error("Error:", error));
-  //   });
-  // });
+  getFolders().then((data) => {
+    console.log(Object.keys(data).length);
+    let treeData = [];
+    for (const [key, value] of Object.entries(data)) {
+      if (value.length == 7) {
+        folderTimestamps.push(toLocalTimeStr(key)[1]);
+        // let node = {
+        //   text: toLocalTimeStr(key)[0],
+        //   icon: "fa fa-folder",
+        //   nodes: [
+        //     {
+        //       text: "cam-401",
+        //       icon: "fa-regular fa-image",
+        //       id: key + " cam401",
+        //       class: "cam_item",
+        //     },
+        //     {
+        //       text: "cam-404",
+        //       icon: "fa-regular fa-image",
+        //       id: key + " cam404",
+        //       class: "cam_item",
+        //     },
+        //     {
+        //       text: "cam-405",
+        //       icon: "fa-regular fa-image",
+        //       id: key + " cam405",
+        //       class: "cam_item",
+        //     },
+        //     {
+        //       text: "cam-407",
+        //       icon: "fa-regular fa-image",
+        //       id: key + " cam407",
+        //       class: "cam_item",
+        //     },
+        //     {
+        //       text: "cam-408",
+        //       icon: "fa-regular fa-image",
+        //       id: key + " cam408",
+        //       class: "cam_item",
+        //     },
+        //     {
+        //       text: "cam-409",
+        //       icon: "fa-regular fa-image",
+        //       id: key + " cam409",
+        //       class: "cam_item",
+        //     },
+        //     {
+        //       text: "cam-410",
+        //       icon: "fa-regular fa-image",
+        //       id: key + " cam410",
+        //       class: "cam_item",
+        //     },
+        //   ],
+        // };
+        // treeData.push(node);
+      }
+    }
+    
+    folderTimestamps.sort((a,b) => a - b);
+    console.log(folderTimestamps);
+    console.log(folderTimestamps.length);
+    $("#tree").bstreeview({
+      data: treeData,
+    });
+    $(".cam_item").on("click", function (event) {
+      console.log(event.target.id);
+      event.stopPropagation();
+      [timeStamp, filename] = event.target.id.split(" ");
+      fetch(
+        `https://records-np04-slow-control.app.cern.ch/files/${timeStamp}/${filename}`
+      )
+        .then((response) => response.text())
+        .then((data) => {
+          const element = document.getElementById("screenshot");
+          if (element) {
+            element.remove();
+          }
+          const img = document.createElement("img");
+          img.setAttribute("id", "screenshot");
+          img.src = "data:image/jpeg;base64," + data;
+          document.getElementsByClassName("img_wrapper")[0].appendChild(img);
+        })
+        .catch((error) => console.error("Error:", error));
+    });
+  });
 
   let startDateTime = "";
   let endDateTime = "";
@@ -209,13 +209,13 @@ document.addEventListener("DOMContentLoaded", () => {
   });
   $("#submit_timerange").on("click", function (event) {
     if (startDateTime && endDateTime) {
-      console.log(`${startDateTime} --- ${endDateTime}`);
-      const sd = new Date(startDateTime);
-      console.log(sd);
-      console.log(sd/getTime());
-      const ed = new Date(endtDateTime);
-      console.log(ed);
-      console.log(ed/getTime());
+      const startTimeStamp = new Date(startDateTime);
+      const endTimeStamp = new Date(endDateTime);
+      const timeStampRange = filterByRange(folderTimestamps, startTimeStamp, endTimeStamp);
+      console.log(timeStampRange);
+      console.log(timeStampRange.length);
+
+      
     }
   });
 
