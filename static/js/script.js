1230387a20ae0cfb7385c804e8fadef8f722cf66
Ashot Hambardzumyan <avhmail@gmail.com>
2024-12-05 07:32:15 +0400

    05.12.24

--- static/js/script.js
+++ static/js/script.js
@@ -1,105 +1,47 @@
-// converts timestamps UTC (2024-03-06_18-23-22) local time in milliseconds
-function toLocalUnixTime(timeStr) {
-  let str = timeStr;
-  [date, time] = str.split("_");
-  time = time.replaceAll("-", ":");
-  str = date + " " + time;
-  utcDate = new Date(str);
-  const localDate = new Date(
-    utcDate.getTime() - utcDate.getTimezoneOffset() * 60000
-  );
+/**
+ * Converts a local time string (YYYY_MM_DD_HH_mm) to UTC format
+ * @param {string} localTime - Local time string in the format YYYY_MM_DD_HH_mm
+ * @returns {string} - UTC time string in the same format
+ */
+function localToUTC(localTime) {
+  const [year, month, day, hour, minute] = localTime.split("_").map(Number);
 
-  const localUnixTimeStamp = localDate.getTime();
-  return localUnixTimeStamp;
+  // Create a Date object in local time
+  const localDate = new Date(year, month - 1, day, hour, minute);
+
+  // Convert to UTC and format the result
+  const utcYear = localDate.getUTCFullYear();
+  const utcMonth = String(localDate.getUTCMonth() + 1).padStart(2, "0");
+  const utcDay = String(localDate.getUTCDate()).padStart(2, "0");
+  const utcHour = String(localDate.getUTCHours()).padStart(2, "0");
+  const utcMinute = String(localDate.getUTCMinutes()).padStart(2, "0");
+
+  return `${utcYear}_${utcMonth}_${utcDay}_${utcHour}_${utcMinute}`;
 }
 
-//converts unix timestamp in milliseconds to timestamps as folder names are, like this, 2024-03-08_11-42-02
-function localToUTCTimeStamp(localUnixDate) {
-  const date = new Date(localUnixDate);
-  const str = date
-    .toISOString()
-    .slice(0, 19)
-    .replaceAll("T", "_")
-    .replaceAll(":", "-");
-  return str;
+/**
+ * Converts a UTC time string (YYYY_MM_DD_HH_mm) to local time
+ * @param {string} utcTime - UTC time string in the format YYYY_MM_DD_HH_mm
+ * @returns {string} - Local time string in the same format
+ */
+function utcToLocal(utcTime) {
+  const [year, month, day, hour, minute] = utcTime.split("_").map(Number);
+
+  // Create a Date object in UTC
+  const utcDate = new Date(Date.UTC(year, month - 1, day, hour, minute));
+
+  // Convert to local time and format the result
+  const localYear = utcDate.getFullYear();
+  const localMonth = String(utcDate.getMonth() + 1).padStart(2, "0");
+  const localDay = String(utcDate.getDate()).padStart(2, "0");
+  const localHour = String(utcDate.getHours()).padStart(2, "0");
+  const localMinute = String(utcDate.getMinutes()).padStart(2, "0");
+
+  return `${localYear}_${localMonth}_${localDay}_${localHour}_${localMinute}`;
 }
-//formats local time unix timestamp in milliseconds
-function localTimeFormatted(localUnixDate) {
-  const date = new Date(localUnixDate);
-  const str = date.toString().slice(4, 31);
-  return str;
-}
+console.log(utcToLocal('2024_12_04_01_00'));
 
-//receives sorted array (sortedArr) of integer numbers,
-//and returns new array of elements between min and max
-// function filterByRange(sortedArr, minValue, maxValue) {
-//   const len = sortedArr.length;
-//   let startIndex = 0;
-//   let endIndex = len - 1;
-//   if (sortedArr[startIndex] < minValue) {
-//     let tempStartIndex = startIndex;
-//     let tempEndIndex = endIndex;
-//     let middleIndex;
 
-//     do {
-//       middleIndex = Math.floor((tempEndIndex + tempStartIndex) / 2);
-//       if (sortedArr[middleIndex] == minValue) {
-//         startIndex = middleIndex;
-//         break;
-//       } else if (sortedArr[middleIndex] > minValue) {
-//         tempEndIndex = middleIndex;
-//       } else if (sortedArr[middleIndex] < minValue) {
-//         tempStartIndex = middleIndex;
-//       }
-//       if (tempEndIndex - tempStartIndex <= 1) {
-//         startIndex = tempStartIndex;
-
-//         break;
-//       }
-//     } while (true);
-//   }
-//   if (sortedArr[endIndex] > maxValue) {
-//     let tempStartIndex = startIndex;
-//     let tempEndIndex = endIndex;
-//     let middleIndex;
-
-//     do {
-//       middleIndex = Math.ceil((tempEndIndex + tempStartIndex) / 2);
-//       if (sortedArr[middleIndex] == maxValue) {
-//         endIndex = middleIndex;
-//         break;
-//       } else if (sortedArr[middleIndex] > maxValue) {
-//         tempEndIndex = middleIndex;
-//       } else if (sortedArr[middleIndex] < maxValue) {
-//         tempStartIndex = middleIndex;
-//       }
-//       if (tempEndIndex - tempStartIndex <= 1) {
-//         endIndex = tempEndIndex;
-
-//         break;
-//       }
-//     } while (true);
-//   }
-//   return sortedArr.slice(startIndex, endIndex + 1);
-// }
-
-// let folderTimestamps = [];
-
-// async function getFolders() {
-//   try {
-//     const response = await fetch("/get_folders");
-//     if (!response.ok) {
-//       throw new Error("Network response was not ok");
-//     }
-//     const data = await response.json();
-//     return data;
-//   } catch (error) {
-//     console.error("Error fetching data:", error);
-//     return null;
-//   }
-// }
-//constructs array of objects for treeview from unix timestamps
-//in milliseconds
 function constructTreeData(arrayOfFileNames) {
   let treeData = [];
   for (let fullPath of arrayOfFileNames) {
@@ -114,16 +56,6 @@ function constructTreeData(arrayOfFileNames) {
   return treeData;
 }
 document.addEventListener("DOMContentLoaded", () => {
-  folderTimestamps = []; //UNIX time in milliseconds
-  let treeData = [];
-
-  
-    // folderTimestamps.sort((a, b) => a - b);
-    // treeData = constructTreeData(folderTimestamps.slice(-30));
-   
-
-
-
   let startDateTime = "";
   let endDateTime = "";
   jQuery("#startDateTime").datetimepicker({
@@ -138,8 +70,8 @@ document.addEventListener("DOMContentLoaded", () => {
   });
   $("#submit_timerange").on("click", function (event) {
     if (startDateTime && endDateTime) {
-      const startDate = startDateTime.replaceAll(" ","_").replaceAll(":","_").replaceAll("/","_");
-      const endDate = endDateTime.replaceAll(" ","_").replaceAll(":","_").replaceAll("/","_");
+      const startDate = localToUTC(startDateTime.replaceAll(" ","_").replaceAll(":","_").replaceAll("/","_"));
+      const endDate = localToUTC(endDateTime.replaceAll(" ","_").replaceAll(":","_").replaceAll("/","_"));
       console.log(startDate)
       console.log(endDate)  
       cam_name = "np04_cam401"
@@ -181,44 +113,7 @@ document.addEventListener("DOMContentLoaded", () => {
       .catch(error => {
         console.error('Error fetching data:', error); 
       });
-    
-      const startTimeStamp = new Date(startDateTime);
-      const endTimeStamp = new Date(endDateTime);
       
-   
-
-
-
-      console.log(endTimeStamp);
-
-      // const timeStampRange = filterByRange(
-      //   folderTimestamps,
-      //   startTimeStamp,
-      //   endTimeStamp
-      // );
-      // treeData = constructTreeData(timeStampRange);
-      // $("#tree").remove();
-      // $("#tree_wrapper").append('<div id="tree"></div>');
-      // $("#tree").bstreeview({
-      //   data: treeData,
-      // });
-      // $(".cam_item").on("click", function (event) {
-      //   event.stopPropagation();
-      //   [timeStamp, filename] = event.target.id.split(" ");
-      //   fetch(`/files/${timeStamp}/${filename}`)
-      //     .then((response) => response.text())
-      //     .then((data) => {
-      //       const element = document.getElementById("screenshot");
-      //       if (element) {
-      //         element.remove();
-      //       }
-      //       const img = document.createElement("img");
-      //       img.setAttribute("id", "screenshot");
-      //       img.src = "data:image/jpeg;base64," + data;
-      //       document.getElementsByClassName("img_wrapper")[0].appendChild(img);
-      //     })
-      //     .catch((error) => console.error("Error:", error));
-      // });
     }
   });
 });
